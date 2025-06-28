20773becb34aec47eaaadae191c6c768e76bbd31
Cao Thanh Tam <tam.ct@intelin.vn>
2025-06-28 16:52:16 +0700

    fix: deployment issues for GitHub Pages

--- app/components/client-page-wrapper.tsx
+++ app/components/client-page-wrapper.tsx
@@ -98,10 +98,10 @@ export default function ClientPageWrapper() {
         <div className="container flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
           <p className="text-xs text-gray-500 dark:text-gray-400">© 2025 Thành Tâm. {t('allRightsReserved')}</p>
           <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-            <Link className="text-xs hover:underline underline-offset-4" href="/privacy">
+            <Link className="text-xs hover:underline underline-offset-4" href="/privacy/">
               {t('privacyPolicy')}
             </Link>
-            <Link className="text-xs hover:underline underline-offset-4" href="/terms">
+            <Link className="text-xs hover:underline underline-offset-4" href="/terms/">
               {t('termsConditions')}
             </Link>
           </nav>
